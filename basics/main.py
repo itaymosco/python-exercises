57f40ddca4c5d9cb8c62ae819a3c8f3ef48b2c77
itay <itaymosco@gmail.com>
2024-02-14 09:08:41 +0200

    itay last commit

---
+++ basics/main.py
@@ -0,0 +1,6 @@
+num_1 = int(input("choose the first number: "))
+num_2 = int(input("choose the first number: "))
+if (num_1 * num_2) < 1000:
+    print(num_1 * num_2)
+else:
+    print(num_1 + num_2)
